8c5b6e3a0efecfc2b478d4f3d137bfd894afaebf
Misato-Seki <116722027+Misato-Seki@users.noreply.github.com>
2024-04-18 13:32:03 +0300

    renamed:    top.jpg -> images/top.jpg modified:   index.html modified:   router.js

--- router.js
+++ router.js
@@ -83,7 +83,7 @@ module.exports = {
                 renderCSS('./index.css', response);
                 break;
             case '/images/top.jpg' :
-                renderIMG('./top.jpg', response);
+                renderIMG('./images/top.jpg', response);
                 break;
             // 2. about page
             case '/about':
